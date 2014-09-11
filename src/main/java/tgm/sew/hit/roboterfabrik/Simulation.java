54676581a2a99a009dd70e63cc47400ea1d699d4
Rene8888 <rene.hollander@hotmail.de>
2014-09-11 19:18:31 +0200

    fixed log4j2 config file location updated gitignore added new run config

--- src/main/java/tgm/sew/hit/roboterfabrik/Simulation.java
+++ src/main/java/tgm/sew/hit/roboterfabrik/Simulation.java
@@ -1,8 +1,13 @@
 package tgm.sew.hit.roboterfabrik;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Simulation {
 
+	private static final Logger logger = LogManager.getLogger(Simulation.class);
+
 	public static void main(String[] args) {
-		System.out.println("lol");
+		logger.info("Starting Roboterfabric");
 	}
 }
