8826cf6930f81e553b629fe601bbd608bdfe4996
Rene8888 <rene.hollander@hotmail.de>
2014-09-12 20:22:26 +0200

    finished comments

--- src/main/java/tgm/sew/hit/roboterfabrik/Simulation.java
+++ src/main/java/tgm/sew/hit/roboterfabrik/Simulation.java
@@ -57,6 +57,7 @@ public class Simulation {
 		this.warehousePath = warehousePath;
 		this.logFilePath = logFilePath;
 
+		// configure logger to ouput to the specified path
 		configureLogger(new File(this.logFilePath, "roboterfabrik.log"));
 	}
 
@@ -71,6 +72,46 @@ public class Simulation {
 	@Override
 	public String toString() {
 		return "Simulation [duration=" + duration + ", employeeCount=" + employeeCount + ", supplierCount=" + supplierCount + ", warehousePath=" + warehousePath + ", logFilePath=" + logFilePath + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + duration;
+		result = prime * result + employeeCount;
+		result = prime * result + ((logFilePath == null) ? 0 : logFilePath.hashCode());
+		result = prime * result + supplierCount;
+		result = prime * result + ((warehousePath == null) ? 0 : warehousePath.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Simulation other = (Simulation) obj;
+		if (duration != other.duration)
+			return false;
+		if (employeeCount != other.employeeCount)
+			return false;
+		if (logFilePath == null) {
+			if (other.logFilePath != null)
+				return false;
+		} else if (!logFilePath.equals(other.logFilePath))
+			return false;
+		if (supplierCount != other.supplierCount)
+			return false;
+		if (warehousePath == null) {
+			if (other.warehousePath != null)
+				return false;
+		} else if (!warehousePath.equals(other.warehousePath))
+			return false;
+		return true;
 	}
 
 	/**
@@ -123,12 +164,16 @@ public class Simulation {
 			if (cmd.hasOption("help")) {
 				printHelpAndExit(helpFormatter, options, 0);
 			}
+
+			// read all values from the cli and parse them
 			String lagerOptionValue = (String) cmd.getParsedOptionValue("lager");
 			String logsOptionValue = (String) cmd.getParsedOptionValue("logs");
 			int lieferantenOptionValue = ((Number) cmd.getParsedOptionValue("lieferanten")).intValue();
 			int monteureOptionValue = ((Number) cmd.getParsedOptionValue("monteure")).intValue();
 			int laufzeitOptionValue = ((Number) cmd.getParsedOptionValue("laufzeit")).intValue();
 
+			// check if lagerDir is not a file and create all direcotries
+			// log an error and stop the app if the path is not valid
 			File lagerDir = new File(lagerOptionValue);
 			lagerDir.mkdirs();
 			if (!lagerDir.isDirectory()) {
@@ -136,6 +181,8 @@ public class Simulation {
 				printHelpAndExit(helpFormatter, options, 1);
 			}
 
+			// check if loggingDir is not a file and create all direcotries
+			// log an error and stop the app if the path is not valid
 			File loggingDir = new File(logsOptionValue);
 			loggingDir.mkdirs();
 			if (!loggingDir.isDirectory()) {
@@ -143,16 +190,22 @@ public class Simulation {
 				printHelpAndExit(helpFormatter, options, 1);
 			}
 
+			// check if more then 1 suppliers are given
+			// if less log to console and stop application
 			if ((lieferantenOptionValue >= 1) == false) {
 				LOGGER.error("you have to specify more or equal to 1 suppliers");
 				printHelpAndExit(helpFormatter, options, 1);
 			}
 
+			// check if more then 1 mechanics are given
+			// if less log to console and stop application
 			if ((monteureOptionValue >= 1) == false) {
 				LOGGER.error("you have to specify more or equal to 1 mechanics");
 				printHelpAndExit(helpFormatter, options, 1);
 			}
 
+			// create a new simulation and start it
+			// if an error occurs log to console and exit applications
 			try {
 				Simulation sim = new Simulation(laufzeitOptionValue, monteureOptionValue, lieferantenOptionValue, lagerDir, loggingDir);
 				sim.start();
@@ -172,27 +225,37 @@ public class Simulation {
 	}
 
 	private static void printHelpAndExit(HelpFormatter helpFormatter, Options options, int exitValue) {
+		// print help dialog to console and exit app with given error code
 		helpFormatter.printHelp("java - jar roboterfabrik.jar", "Verfuegbare Parameter:", options, null, true);
 		System.exit(exitValue);
 	}
 
 	private static void addOption(Options options, String opt, String desc, Class<?> type, boolean required, boolean hasArg) {
+		// add an option to the given options
+		// just a small wrapper around OptionBuilder for easier usage
 		OptionBuilder.withDescription(desc);
 		if (type != null) {
+			// type to cast cli value to
 			OptionBuilder.withType(type);
 		}
 		if (required) {
+			// if set to required, an error will occur if the options is not set
 			OptionBuilder.isRequired();
 		}
 		if (hasArg) {
+			// specify that the options needs and argument following it
 			OptionBuilder.hasArg();
 		}
+		// build option and add to options
 		Option o = OptionBuilder.create(opt);
 		options.addOption(o);
 	}
 
 	private static void configureLogger(File logFile) {
+		// set logFilename property used by log4j2.xml so you can specify it
+		// during runtime
 		System.setProperty("logFilename", logFile.getAbsolutePath());
+		// get context from logger and reconfigure it
 		LoggerContext context = (LoggerContext) LogManager.getContext(false);
 		context.reconfigure();
 	}
