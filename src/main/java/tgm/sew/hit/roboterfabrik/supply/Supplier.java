301d7cdd07eadc219370a23afd7f92c68c99761d
Rene8888 <rene.hollander@hotmail.de>
2014-09-13 17:22:25 +0200

    fixed umlaut and wrong logger class in supplier to be able to build project

--- src/main/java/tgm/sew/hit/roboterfabrik/supply/Supplier.java
+++ src/main/java/tgm/sew/hit/roboterfabrik/supply/Supplier.java
@@ -3,24 +3,20 @@ package tgm.sew.hit.roboterfabrik.supply;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import tgm.sew.hit.roboterfabrik.Simulation;
-import tgm.sew.hit.roboterfabrik.Warehouser;
-
 /**
- * Der Lieferant liefert neue Teile welche von der Klasse 
- * Supply erstellt wurden an den Lagermitarbeiter.
- * Mehrere Lieferanten koennen Lieferungen von verschiedenen Teilen und
- * verschiedener Anzahl an Teilen an den Lagermitarbeiter übergeben.
+ * Der Lieferant liefert neue Teile welche von der Klasse Supply erstellt wurden
+ * an den Lagermitarbeiter. Mehrere Lieferanten koennen Lieferungen von
+ * verschiedenen Teilen und verschiedener Anzahl an Teilen an den
+ * Lagermitarbeiter uebergeben.
  * 
  * @author Samuel Schober
  *
  */
 public class Supplier {
 
-	private static final Logger LOGGER = LogManager.getLogger(Simulation.class);
+	private static final Logger LOGGER = LogManager.getLogger(Supplier.class);
 
 	private boolean running;
-	
 
 	public Supplier() {
 		this.running = true;
@@ -28,7 +24,7 @@ public class Supplier {
 
 	public void run() {
 		while (this.running) {
-			
+
 		}
 	}
 
