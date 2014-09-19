470b3330fe2e473117d4a195f9fe439840665571
Simon <swortha@student.tgm.ac.at>
2014-09-19 21:10:48 +0200

    Implementierung von Office. Implementierung von Threadee. Dokumentation upgedated.

--- src/main/java/tgm/sew/hit/roboterfabrik/Office.java
+++ src/main/java/tgm/sew/hit/roboterfabrik/Office.java
@@ -3,16 +3,41 @@ package tgm.sew.hit.roboterfabrik;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ *Das Office verteilt IDs an alle Arbeiter (Employee) und Threadees.
+ *Jeder Arbeiter bekommt eine eindeutige ID.
+ *Jeder Threadee bekommt eine eindeutige ID.
+ * 
+ * @author Simon Wortha
+ */
 public class Office {
-
+	private int workerId;
+	private int threadeeId;
 	private static final Logger LOGGER = LogManager.getLogger(Office.class);
-
-	public int generateWorkerID() {
-		return 0;
+	
+	/**
+	 * Erzeugt ein neues Office
+	 * Setzt beim Start zuerst alle IDs auf Null
+	 */
+	public Office() {
+		this.workerId = 0;
+		this.threadeeId = 0;
 	}
-
+	/**
+	 * Das Office erstellt die ID für einen Worker (und zählt immer +1 dazu)
+	 * 
+	 * @return gibt einen int Wert, bzw. die ID für einen Worker zurück
+	 */
+	public int generateWorkerID() {
+		return ++workerId;
+	}
+	/**
+	 * Das Office erstellt die ID für einen Threadee (und zählt immer +1 dazu)
+	 * 
+	 * @return gibt einen int Wert, bzw. die ID für einen Threadde zurück
+	 */
 	public int generateThreadeeID() {
-		return 0;
+		return ++threadeeId;
 	}
 
 }
--- src/main/java/tgm/sew/hit/roboterfabrik/Threadee.java
+++ src/main/java/tgm/sew/hit/roboterfabrik/Threadee.java
@@ -7,6 +7,13 @@ import org.apache.logging.log4j.Logger;
 
 import tgm.sew.hit.roboterfabrik.part.Part;
 
+/**
+ * Der Threadee ist der Roboter, der von den Employees (Arbeitern) angefertigt wird.
+ * Er besteht aus mehreren Teilen (Hier parts) die zur Zusammensetzung des Roboters benötigt werden. Diese muss der Arbeiter,
+ * vorher von dem Supplier abholen.
+ * 
+ * @author Simon Wortha
+ */
 public class Threadee {
 
 	private static final Logger LOGGER = LogManager.getLogger(Threadee.class);
@@ -14,21 +21,30 @@ public class Threadee {
 	private int id;
 	private Employee creator;
 	private ArrayList<Part> parts;
-
+	
+	/**
+	 * Erstellt einen neuen Threadee
+	 * 
+	 * @param id 		ist die ID die dem fertigen Threadee zugewiesen wird.
+	 * @param creator 	ist der Employee der den Threadee zusammengebaut hat.
+	 * @param parts 	ist eine ArrayList, die die ganzen Teile die für den Roboter benötigt werden.
+	 */
 	public Threadee(int id, Employee creator, ArrayList<Part> parts) {
-
+		this.id = id;
+		this.creator = creator;
+		this.parts = parts;
 	}
 
 	public int getID() {
-		return 0;
+		return id;
 	}
 
 	public Employee getCreator() {
-		return null;
+		return creator;
 	}
 
 	public ArrayList<Part> getParts() {
-		return null;
+		return parts;
 	}
 
 }
