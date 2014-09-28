db80735c7f67f980ba3f27d07e054619bea6db79
Samuel Schober <sschober@student.tgm.ac.at>
2014-09-28 11:28:01 +0200

    Added Employee Test Class

---
+++ src/test/java/tgm/sew/hit/roboterfabrik/test/EmployeeTest.java
@@ -0,0 +1,14 @@
+package tgm.sew.hit.roboterfabrik.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmployeeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
