e2cac126b07ae39e2e71767eb05c8a3880bb682b
Samuel Schober <sschober@student.tgm.ac.at>
2014-10-01 11:20:59 +0200

    Added Test Cases to WatchdogTest

--- src/test/java/tgm/sew/hit/roboterfabrik/test/WatchdogTest.java
+++ src/test/java/tgm/sew/hit/roboterfabrik/test/WatchdogTest.java
@@ -2,13 +2,35 @@ package tgm.sew.hit.roboterfabrik.test;
 
 import static org.junit.Assert.*;
 
+import javax.imageio.spi.RegisterableService;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import tgm.sew.hit.roboterfabrik.watchdog.Watchable;
+import tgm.sew.hit.roboterfabrik.watchdog.Watchdog;
 
 public class WatchdogTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateWatchdog() {
+		Watchdog w = new Watchdog(10);
+	}
+
+	@Test
+	public void testStartWatchdog() {
+		Watchdog w = new Watchdog(10);
+		Watchable wa = new Watchable() {
+
+			@Override
+			public void stop() {
+				// TODO Auto-generated method stub
+
+			}
+		};
+		w.registerWatchable(wa);
+		w.startWatchdog();
 	}
 
 }
